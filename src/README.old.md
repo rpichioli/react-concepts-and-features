fd526edf749dfeb4aa16ee50665ad2dd1663aa88
Rodrigo Quiñones Pichioli <rodrigo.q.p@hotmail.com>
2019-01-18 10:52:24 -0200

    Cleaning repository, changing app objectives

--- src/README.old.md
+++
@@ -1,2 +0,0 @@
-# react-concepts-and-features
->In development phase
--- src/src/containers/App/App.js
+++ src/src/containers/App/App.js
@@ -2,30 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-			<div className='ui container segment'>
-				<h1>React Concepts and Features</h1>
-				<div className='ui divider'></div>
+	render() {
+		return (
+			<div className='container'>
+				<h1>React -  In progress</h1>
 			</div>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Learn React
-      //     </a>
-      //   </header>
-      // </div>
-    );
-  }
+		);
+	}
 }
 
 export default App;
