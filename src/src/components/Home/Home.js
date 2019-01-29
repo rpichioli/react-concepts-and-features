a968db854a0cab8bea6e8174131ba4df7aa0bd92
Rodrigo Quiñones Pichioli <rodrigo.q.p@hotmail.com>
2019-01-29 11:06:00 -0200

    Cleaned repo from unused packages/files, applyed Bootstrap and Routing

--- src/src/components/Home/Home.js
+++ src/src/components/Home/Home.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 class Home extends React.Component {
 	render() {
-		return (<div>
-			<h1>Home</h1>
-		</div>);
+		return (
+			<div>
+				<h1>SPA using React + Redux</h1>
+				<p>Temporary page, the project development is in progress</p>
+			</div>
+		);
 	}
 }
 
--- src/src/index.js
+++ src/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
+//-------------- Routing --------------
+import { Switch, Route, Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+//-------------- Bootstrap --------------
+import 'bootstrap/dist/css/bootstrap.min.css';
+//-------------- Components --------------
+import App from './containers/App/App';
+import Home from './components/Home/Home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Browser history
+const history = createBrowserHistory();
+
+ReactDOM.render(
+	<Router history={history}>
+		<App>
+			<Switch>
+				<Route exact to="/" component={Home} />
+			</Switch>
+		</App>
+	</Router>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
