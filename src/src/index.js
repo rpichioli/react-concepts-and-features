e092a3fc7f1a1fbb11b4cc2e3a0c685cb4f30fbc
Rodrigo Quiñones Pichioli <rodrigo.q.p@hotmail.com>
2019-01-29 16:24:04 -0200

    Configured Redux store, Redux middleware, blank action and blank reducer

--- src/src/index.js
+++ src/src/index.js
@@ -4,23 +4,34 @@ import * as serviceWorker from './serviceWorker';
 //-------------- Routing --------------
 import { Switch, Route, Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+//-------------- Redux --------------
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+//-------------- Redux --------------
+import rootReducer from './reducers/rootReducer';
 //-------------- Bootstrap --------------
 import 'bootstrap/dist/css/bootstrap.min.css';
 //-------------- Components --------------
 import App from './containers/App/App';
 import Home from './components/Home/Home';
 
-// Browser history
+// Browser history for routing configuration
 const history = createBrowserHistory();
+// Redux store -> Combined reducers and middleware for debugging
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
-	<Router history={history}>
-		<App>
-			<Switch>
-				<Route exact to="/" component={Home} />
-			</Switch>
-		</App>
-	</Router>
+	<Provider store={store}>
+		<Router history={history}>
+			<App>
+				<Switch>
+					<Route exact to="/" component={Home} />
+				</Switch>
+			</App>
+		</Router>
+	</Provider>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
