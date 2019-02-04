e3eec0b0f2e358b97eb341c54aa4481446364937
Rodrigo Quiñones Pichioli <rodrigo.q.p@hotmail.com>
2019-02-04 10:41:00 -0200

    Action e reducer code bases done, ready for next movement within React

--- src/src/actions/users.js
+++ src/src/actions/users.js
@@ -1,22 +1,65 @@
 //import axios from 'axios';
 
-// ----- Constants -----
+// ----- Constants -----------------------------------------------------
 export const SET_USERS = 'SET_USERS';
+export const ADDED_USER = 'ADDED_USER';
+export const UPDATED_USER = 'UPDATED_USER';
+export const REMOVED_USER = 'REMOVED_USER';
 
-// ----- Actions -----
-export function setProjects(users) { return { type: SET_USERS, users }}
+// ----- Actions -------------------------------------------------------
+export function setUser(users) { return { type: SET_USERS, users }}
+export function addedUser(users) { return { type: ADDED_USER, users }}
+export function updatedUser(users) { return { type: UPDATED_USER, users }}
+export function removedUser(users) { return { type: REMOVED_USER, users }}
 
-// ----- Fetch and action dispatchs -----
+// ----- Fetch and action dispatchs ------------------------------------
+/**
+ * Fetch all users
+ */
 export function fetchProjects() {
-	// TODO - Get data from localStorage
-	let users = [
-		{name: 'Filomena Silva'},
-		{name: 'Eliseo Gonçalves'},
-		{name: 'Marcos Roncador'}
-	];
+	return (dispatch, getState) => dispatch(setUser(getMockedData()));
+}
 
-	// Return dispatched data
-	return (dispatch, getState) => {
-		dispatch(setProjects(users))
-	};
+/**
+ * Add new user
+ */
+export function addProjects(user) {
+	return (dispatch, getState) => dispatch(addedUser(getMockedData()));
+}
+
+/**
+ * Update usr by ID
+ */
+export function updateProjects(user) {
+	return (dispatch, getState) => dispatch(updatedUser(getMockedData()));
+}
+
+/**
+ * Remove user by ID
+ */
+export function removeProjects(id) {
+	return (dispatch, getState) => dispatch(removedUser(getMockedData()));
+}
+
+/**
+ * Get the last existing ID from mock - Just for tests
+ */
+export function fetchLastID() {
+	let users = getMockedData();
+	return users.reduce((prev, current) => (prev.y > current.y) ? prev : current);
+}
+
+// ----- Internal ------------------------------------------------------
+/**
+ * Returns static mocked users
+ * @return {array} Array of objects
+ */
+const getMockedData = () => {
+	return [
+		{id: 1, name: 'Filomena Silva'},
+		{id: 2, name: 'Eliseo Gonçalves'},
+		{id: 3, name: 'Marcos Roncador'},
+		{id: 4, name: 'Marcos Roncador'},
+		{id: 5, name: 'Marcos Roncador'}
+	];
 }
--- src/src/reducers/users.js
+++ src/src/reducers/users.js
@@ -1,9 +1,18 @@
-import { SET_USERS } from '../actions/users';
+import { SET_USERS, ADDED_USER, UPDATED_USER, REMOVED_USER } from '../actions/users';
 
 export default function projects(state = [], action = {}) {
 	switch (action.type) {
 		case SET_USERS:
 			return action.users;
+		case ADDED_USER:
+			return [...state, action.user];
+		case UPDATED_USER:
+			return state.map((item, i) => {
+				if (item.id === Number(action.user.id)) item.user = action.user;
+				return item;
+			});
+		case REMOVED_USER:
+			return state.filter(item => item.id !== Number(action.id));
 		default:
 			return state;
 	}
