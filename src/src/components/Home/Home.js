a096cf1e773038c6eedf680a24d4b608f7df3e5f
Rodrigo Quiñones Pichioli <rodrigo.q.p@hotmail.com>
2019-02-06 10:39:03 -0200

    Home structure and content + styles

--- src/src/components/Home/Home.js
+++ src/src/components/Home/Home.js
@@ -4,8 +4,17 @@ class Home extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>SPA using React + Redux</h1>
-				<p>Temporary page, the project development is in progress</p>
+				<h1>Welcome developer!</h1>
+				<div className="card">
+					<div className="card-header">About the project</div>
+					<div className="card-body">
+						<p>Welcome, this is a SPA (single-page application) built with React + Redux.</p>
+						<p>The objective is to show how to mantain a stateful application manipulating data through Redux applying action and reducer concepts.</p>
+						<p>We will not persist database, this project is to illustrate React-Redux interation providing a working stateful application.</p>
+						<p>The sync between Redux store (state mantained from any reducers) and the browser LocalStorage the way you can refresh data will not be lost.</p>
+					</div>
+				</div>
+				
 			</div>
 		);
 	}
